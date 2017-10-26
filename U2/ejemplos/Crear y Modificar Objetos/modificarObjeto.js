50287f7c253665deb006e744ed540382a3a2bf79
Cibeles <cibelesmora@gmail.com>
2017-10-26 18:00:57 +0200

    Ejemplos

--- U2/ejemplos/Crear y Modificar Objetos/modificarObjeto.js
+++ U2/ejemplos/Crear y Modificar Objetos/modificarObjeto.js
@@ -7,19 +7,19 @@ function Empresa(nombre, nombreCEO, favColorCEO){
 }
 var Google = new Empresa("Google","Larry y Sergei","Muchos: rojo, verde, azul y amarillo");
 
-//Podemos cambiar el objeto después de crearlo
-Google.sede = "California";
-console.log(Google);
+// //Podemos cambiar el objeto después de crearlo
+// Google.sede = "California";
+// console.log(Google);
 
 
 // //Modificamos el prototipo de Empresa.
 // //Este cambio afecta a todos!
-Empresa.prototype.tipo = "internacional";
-Empresa.prototype.describe  = function(){
-  alert(this.nombre + " es una empresa " + this.tipo + " fundada por " + this.ceo.nombre);
-}
-//console.log(Google);
-//Google.describe();
+// Empresa.prototype.tipo = "internacional";
+// Empresa.prototype.describe  = function(){
+//   alert(this.nombre + " es una empresa " + this.tipo + " fundada por " + this.ceo.nombre);
+// }
+// //console.log(Google);
+// //Google.describe();
 
 // var Facebook = new Empresa("Facebook", "Mark", "Azul");
 // console.log(Facebook.tipo);
